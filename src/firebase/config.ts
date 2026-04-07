17e1e055b4bfa425bc98a05ed842e3a00fea9524
RONALD MUTAI (Jerikonda) <jerikondaronaldo@gmail.com>
2026-04-07 09:15:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7669751488-174ed",
+  "appId": "1:450807960180:web:96aa568fcd777a4a917721",
+  "apiKey": "AIzaSyB2JJRtDnrRQPmfCf-jFdfKi5iPXtuzJaQ",
+  "authDomain": "studio-7669751488-174ed.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "450807960180"
+};
